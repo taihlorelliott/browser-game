2d1da184ebd84605bb6ce5b88a69c4639559ebae
Taihlor Elliott <taihlor.elliott@gmail.com>
2024-10-25 10:40:15 -0400

    complete

--- app.js
+++ app.js
@@ -36,23 +36,129 @@ yesButton.addEventListener('click', (event) => {
             runs past you and into the bathroom, covered in blood. Do you proceed
             down the hallway?` 
 // next event listener here!
-        }
+        yesButton.addEventListener('click', (event) => {
+            const hallwayYes = function() {
+                 display.textContent = `You hear a loud bang from the kitchen and 
+                 foot steps coming toward you. You live alone. Do you run?` 
+// next event listener here!
+            yesButton.addEventListener('click', (event) => {
+                const kevinYes = function() {
+                    display.textContent = `You start running towards the back door, 
+                    when you remember Kevin. Do you go back for Kevin?` 
+// next event listener here!
+                yesButton.addEventListener('click', (event) => {
+                     const safetyYes = function() {
+                        display.textContent = `Kevin knew you 
+                        would never leave him, after all he is your only friend.  
+                        You dart into the bathroom scoop up Kevin. You run with Kevin 
+                        in your arms and make it to safety!` 
+
+                        optionButtons.forEach((button) => {
+                            button.style.display = "none"; 
+                         })
+// next event listener here!
+}
+//has to go outside of the function "light yes"
+safetyYes();
+});
+}
+//has to go outside of the function "light yes"
+kevinYes();
+noButton.addEventListener('click', (event) => {
+    const abondonNo = function() {
+       display.textContent = `You decided to abandon Kevin, poor choice!
+       Game Over.` 
+
+       optionButtons.forEach((button) => {
+           button.style.display = "none"; 
+        })
+// next event listener here!
+}
+//has to go outside of the function "light yes"
+abondonNo();
+});
+});
+}
+//has to go outside of the function "light yes"
+hallwayYes();
+noButton.addEventListener('click', (event) => {
+    const defendNo = function() {
+       display.textContent = `You decide to face the footsteps.  Which was a poor choice. 
+       You have never take self defence and have no wepons.  Game Over.` 
+
+       optionButtons.forEach((button) => {
+           button.style.display = "none"; 
+        })
+// next event listener here!
+}
+//has to go outside of the function "light yes"
+defendNo();
+});
+});
+}
 //has to go outside of the function "light yes"
 lightYes();
+    noButton.addEventListener('click', (event) => {
+        const aliveYes = function() {
+            display.textContent = `Good idea!  You grab Kevin and run to the back door, 
+            you made it out alive!` 
+
+            optionButtons.forEach((button) => {
+                button.style.display = "none"; 
+             })
+
+// next event listener here!
+}
+//has to go outside of the function "light yes"
+aliveYes();
+});
 });
 }
 //has to go outside of the function "investigate yes"    
 investigateYes();
-});
-// makes "no button" function
 noButton.addEventListener('click', (event) => {
-    firstNo();
-});
-const firstNo = function() {
-    display.textContent = `You decide to think nothing of it and return 
-    to sleep.  A few moments later there is a knock on your bedroom door. 
-    You live alone. Do you hide?` 
+    const escapeNo = function() {
+       display.textContent = `You decide to leave the light off, 
+       you feel your cat Kevin brush passed you.  You decide to not chance it. 
+       If Kevin is running so are you.  You and Kevin escape!` 
+
+       optionButtons.forEach((button) => {
+        button.style.display = "none"; 
+     })
+// next event listener here!
 }
+//has to go outside of the function "light yes"
+excapeNo();
+});
+
+});
+
+noButton.addEventListener('click', (event) => {
+    const investigateNo = function() {
+       display.textContent = `You run to find your cat Kevin. You 
+       scoop him up into your arms and escape to safety!` 
+
+       optionButtons.forEach((button) => {
+           button.style.display = "none"; 
+        })
+// next event listener here!
+}
+//has to go outside of the function "light yes"
+investigateNo();
+});
+
+
+
+
+// makes "no button" function
+// noButton.addEventListener('click', (event) => {
+//     firstNo();
+// });
+// const firstNo = function() {
+//     display.textContent = `You decide to think nothing of it and return 
+//     to sleep.  A few moments later there is a knock on your bedroom door. 
+//     You live alone. Do you hide?` 
+// }
 // makes "second yes" function
 // yesButton.addEventListener('click', (event) => {
 //     secondYes();
